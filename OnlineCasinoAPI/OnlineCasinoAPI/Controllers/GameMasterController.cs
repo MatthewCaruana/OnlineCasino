4b4d20678fec2305fa767f91046071760747c1b4
MatthewCaruana <matthew.caruana99@gmail.com>
2024-02-17 22:09:43 +0100

    Adding Base code to game master controller endpoints

--- OnlineCasinoAPI/OnlineCasinoAPI/Controllers/GameMasterController.cs
+++ OnlineCasinoAPI/OnlineCasinoAPI/Controllers/GameMasterController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OnlineCasino.Application.DTOs;
 using OnlineCasino.Application.Services.Interfaces;
 
 namespace OnlineCasinoAPI.Controllers
@@ -16,6 +17,48 @@ namespace OnlineCasinoAPI.Controllers
         {
             _logger = logger;
             _gameService = gameService;
+        }
+
+        [HttpPost]
+        [Route("CreateGame")]
+        public void CreateGame([FromBody] CreateGameDTO createGame)
+        {
+
+        }
+
+        [HttpPost]
+        [Route("CreateCollection")]
+        public void CreateCollection([FromBody] CreateCollectionDTO createCollection)
+        {
+
+        }
+
+        [HttpPut]
+        [Route("UpdateGame")]
+        public void UpdateGame([FromBody] UpdateGameDTO updateGame)
+        {
+
+        }
+
+        [HttpPut]
+        [Route("UpdateCollection")]
+        public void UpdateCollection([FromBody] UpdateCollectionDTO updateCollection)
+        {
+
+        }
+
+        [HttpDelete]
+        [Route("DeleteGame")]
+        public void DeleteGame([FromBody] int id)
+        {
+
+        }
+
+        [HttpPut]
+        [Route("DeleteCollection")]
+        public void DeleteCollection([FromBody] int id)
+        {
+
         }
     }
 }
